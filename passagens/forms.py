87d9ca41e420df3ff1315a918c40ec5717ce2ba4
Vinicius Alexandre <viniciusalexfernandes@gmail.com>
2022-10-18 18:16:57 -0300

    att

--- passagens/forms.py
+++ passagens/forms.py
@@ -5,6 +5,7 @@ from django.forms import Textarea
 from tempus_dominus.widgets  import  DatePicker
 from datetime import datetime
 from passagens.classes_viagem import tipos_de_classes
+from passagens.validation import *
 
 class PassagemForms(forms.Form):
     nome_sobrenome = forms.CharField(label='Nome e sobrenome', max_length=100)
@@ -22,9 +23,20 @@ class PassagemForms(forms.Form):
     )
     email = forms.EmailField(label='Email', max_length=150)
 
-    def clean_origem(self):
+    def clean(self):
         origem = self.cleaned_data.get('origem')
-        if any(char.isdigit() for char in origem):
-            raise forms.ValidationError('Origem inválida:Não pode conter números')
-        else:
-            return origem
+        destino = self.cleaned_data.get('destino')
+        data_ida = self.cleaned_data.get('data_ida')
+        data_volta = self.cleaned_data.get('data_volta')
+        data_pesquisa = self.cleaned_data.get('data_pesquisa')
+        lista_de_erros = {}
+        campo_tem_algum_numero(origem,'origem', lista_de_erros)
+        campo_tem_algum_numero(destino,'destino', lista_de_erros)
+        campos_nao_podem_ser_iguais(origem, destino , lista_de_erros)
+        data_de_volta_nao_pode_ser_menor_que_data_ida(data_ida, data_volta, lista_de_erros)
+        data_de_ida_nao_pode_ser_menor_que_data_da_pesquisa(data_ida, data_pesquisa, lista_de_erros)
+        if lista_de_erros is not None:
+            for erro in lista_de_erros:
+                mensagem_erro = lista_de_erros[erro]
+                self.add_error(erro, mensagem_erro)
+        return self.cleaned_data
---
+++ passagens/validation.py
@@ -0,0 +1,16 @@
+def campos_nao_podem_ser_iguais(origem, destino, lista_de_erros):
+    '''Verifica se o destino e origem são iguais'''
+    if origem.lower() == destino.lower():
+        lista_de_erros['destino'] = 'Origem e destino não podem ser iguais'
+
+def campo_tem_algum_numero(valor_campo, nome_campo,lista_de_erros):
+    '''Verifica se os campos tem algum digito númerico'''
+    if any(char.isdigit() for char in valor_campo):
+        lista_de_erros[nome_campo] = 'Não pode incluir números neste campo'
+
+def data_de_volta_nao_pode_ser_menor_que_data_ida(data_ida, data_volta, lista_de_erros):
+    if data_ida >= data_volta:
+        lista_de_erros ['data_ida'] = 'Data de ida não pode ser maior que a data de volta'
+def data_de_ida_nao_pode_ser_menor_que_data_da_pesquisa(data_ida, data_pesquisa, lista_de_erros):
+    if data_ida < data_pesquisa:
+        lista_de_erros['data_ida'] = 'Data de ida não pode ser menor que o dia de hoje'
